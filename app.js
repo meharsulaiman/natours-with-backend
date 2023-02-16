ddfcc4bf060787bc1e21aa86973aa091811869d9
mehar sulaiman <dr.love9026@gmail.com>
2023-02-16 11:54:34 +0500

    create an alias like top-5-cheap tours

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  console.log('Hello from middle ware');
-  next();
-});
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
