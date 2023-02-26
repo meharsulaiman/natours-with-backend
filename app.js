f159cf4e34c1c50f564d9bebf3b458e6abe064d8
mehar sulaiman <dr.love9026@gmail.com>
2023-02-26 11:07:49 +0500

    handling unhandled routes

--- app.js
+++ app.js
@@ -16,4 +16,12 @@ app.use(express.static('public'));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+  next();
+});
+
 module.exports = app;
