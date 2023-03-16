6b837d10376e37752a59cfadb53d3d0eb0b62566
mehar sulaiman <dr.love9026@gmail.com>
2023-03-16 18:58:46 +0500

    starts user auth || user model || signup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const authController = require('../controller/authController');
 const userController = require('../controller/userController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router
   .route('/')
