2eed4878e3a3b3f3c9cc1e4e47b2c4cbf8be03e4
mehar sulaiman <dr.love9026@gmail.com>
2023-03-16 09:14:51 +0500

    catching unhandled exceptions and rejections in node js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTIONS: Shutting down...');
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
 dotenv.config({ path: './congif.env' });
 const app = require('./app');
 
@@ -18,13 +25,18 @@ mongoose
   })
   .then(() => {
     console.log('DB connected successfully');
-  })
-  .catch(() => {
-    console.log('DB is in problem');
   });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server is starting on http://localhost:${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION: Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
