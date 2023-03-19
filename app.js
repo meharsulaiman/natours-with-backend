1f31ff06ecb3ad798fb3ced09bd052d784cea21b
mehar sulaiman <dr.love9026@gmail.com>
2023-03-19 21:58:04 +0500

    Setting headers using helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimmit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -10,18 +11,26 @@ const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
 // middleware
+// * Set security HTTP headers
+app.use(helmet());
+
+// * Limit requests from same API 60 per hour
 const limiter = rateLimmit({
   max: 100,
   windowMs: 60 * 60 * 100,
   message: 'Too many requests from this IP, pleas try again in an hour!',
 });
-
 app.use('/api', limiter);
 
+// * Print Logs in developement
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// * Body Parser, reading data from request's body
+app.use(express.json({ limit: '10kb' }));
+
+// * Serving static file {Public folder}
 app.use(express.static('public'));
 
 app.use('/api/v1/tours', tourRouter);
