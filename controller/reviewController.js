9f01dba338b1fbcf4dab42f461840143d18f04f7
mehar sulaiman <dr.love9026@gmail.com>
2023-03-29 19:53:18 +0500

    lecture no 14 of advance mongoose

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -2,7 +2,11 @@ const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllReview = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
