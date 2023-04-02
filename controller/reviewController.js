7e6ad2f8410095cae38a4f9fa6fca3cea15c6530
mehar sulaiman <dr.love9026@gmail.com>
2023-04-02 23:32:51 +0500

    change create and update controllers into factory functions

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -19,20 +19,13 @@ exports.getAllReview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   // Allow nested Routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
-
-  const newReview = await Review.create(req.body);
   next();
+};
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview,
-    },
-  });
-});
-
+exports.createReview = factory.createOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
+exports.updateReview = factory.updateOne(Review);
