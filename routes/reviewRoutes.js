61fc06623106a6ef4607cc75988309b98395eddb
mehar sulaiman <dr.love9026@gmail.com>
2023-04-03 15:45:47 +0500

    finalize authentication and authrization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,11 +5,12 @@ const authController = require('../controller/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReview)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -18,7 +19,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('admin', 'user'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('admin', 'user'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
