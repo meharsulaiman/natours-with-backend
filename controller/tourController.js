63a8c1d9b52b0cb88b778712b6d9395eb32be3ee
mehar sulaiman <dr.love9026@gmail.com>
2023-04-05 21:39:59 +0500

    Geo sptial data

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,6 +1,7 @@
 /* eslint-disable arrow-body-style */
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 exports.aliasTopTours = (req, res, next) => {
@@ -88,6 +89,34 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     length: plan.length,
     data: {
       plan,
+    },
+  });
+});
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please Provide latitude and longitude in the format lat,lng',
+        400
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      data: tours,
     },
   });
 });
