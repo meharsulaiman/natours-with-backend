0ba8bd1d2ac121c0608c5e3979fdb3f8a6d21799
mehar sulaiman <dr.love9026@gmail.com>
2023-04-05 16:45:51 +0500

    final touches

--- models/tourModel.js
+++ models/tourModel.js
@@ -52,6 +52,7 @@ const toursSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
     },
 
     ratingsQuantity: { type: Number, default: 0 },
